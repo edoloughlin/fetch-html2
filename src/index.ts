aa654d80dc77f17963ea7cd3811730aa570293d8
Ed O'Loughlin <391848+edoloughlin@users.noreply.github.com>
2025-06-07 23:57:37 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export default {
 	async fetch(request: Request): Promise<Response> {
-		/**
-		 * Replace `remote` with the host you wish to send requests to
-		 */
-		const remote = "https://example.com";
-
-		return await fetch(remote, request);
+		const url = new URL(request.url);
+  		const l = url.searchParams.get('l');
+		if(l) {
+			const remote = "https://www.lobbying.ie/return/"+l;
+			return await fetch(remote, request);
+		}
+		return new Response(null, { status: 204 }); // 204 No Content is very "light"
 	},
 };
